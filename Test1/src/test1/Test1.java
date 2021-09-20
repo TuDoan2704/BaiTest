8160d231496560ac4163472ef87dcc0f3f204061
TuDoan2704 <doantund2812@gmail.com>
2021-09-20 16:23:21 +0700

    Em đã sửa lại

--- Test1/src/test1/Test1.java
+++ Test1/src/test1/Test1.java
@@ -14,31 +14,31 @@ public class Test1 {
         System.out.print("Nhap so dien ban can tinh: " );
         int n = sc.nextInt();
         int money;
-        if(0< n && n <= 50){
-            money = 1678*n;
+        if(0 < n && n <= 50){
+            money = 1678 * n;
             System.out.println("So tien ban phai tra la: " + money);
         }
-        if(51<= n && n <=100){
-            money = 1678*50 + 1734*(n - 50);           
+        if(51 <= n && n <= 100){
+            money = 1678 * 50 + 1734 * (n - 50);           
             System.out.println("So tien ban phai tra la: " + money);
         }
-        if(101<= n && n <=200){
-            money = 1678*50 + 1734*50 + 2014*(n-100);
+        if(101 <= n && n <= 200){
+            money = 1678 * 50 + 1734 * 50 + 2014 * (n-100);
             
             System.out.println("So tien ban phai tra la: " + money);
         }
-        if(201<= n && n <=300){
-            money = 1678*50 + 1734*50 + 2014*100 + 2536*(n-200);
+        if(201 <= n && n <= 300){
+            money = 1678 * 50 + 1734 * 50 + 2014 * 100 + 2536 * (n-200);
             
             System.out.println("So tien ban phai tra la: " + money);
         }
-        if(301<= n && n <=400){
-            money = 1678*50 + 1734*50 + 2014*100 + 2536*100 + 2834*(n - 300) ;         
+        if(301 <= n && n <= 400){
+            money = 1678 * 50 + 1734 * 50 + 2014 * 100 + 2536 * 100 + 2834 * (n - 300) ;         
             
             System.out.println("So tien ban phai tra la: " + money);
         }
-        if(n>=401){
-            money = 1678*50 + 1734*50 + 2014*100 + 2536*100 + 2834*100 + 2927*(n-400);                      
+        if(n >= 401){
+            money = 1678 * 50 + 1734 * 50 + 2014 * 100 + 2536 * 100 + 2834 * 100 + 2927 *(n-400);                      
             System.out.println("So tien ban phai tra la: " + money);
         }
     }
